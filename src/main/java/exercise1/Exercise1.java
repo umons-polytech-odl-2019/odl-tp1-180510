de50b830a3735d3f9b97343544d3838fa2ea56f7
180510 <180510@umons.ac.be>
2019-10-23 15:03:16 +0200

    Exercice 1 Séance TP2

--- src/main/java/exercise1/Exercise1.java
+++ src/main/java/exercise1/Exercise1.java
@@ -1,16 +1,27 @@
 package exercise1;
 
+import exercise1.Person;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.*;
+
 public class Exercise1 {
 	static Person createPerson(String name, int age) {
 		// Ajoutez les champs name et age à la classe Person.
 		// Créez un constructeur public permettant d'initialiser ces valeurs au moment de la construction.
+		Person temp = new Person(name, age);
 		// Créez des getters publics pour lire ces valeurs une fois la classe construite.
-		return null;
+		return temp;
 	}
 
 	public static void main(String[] args) {
 		// Considérant que cette classe est démarrée en ligne de commande avec un premier paramètre donnant le nom
 		// et un second donnant l'âge (nombre entier), créez un objet Person sur base de ceux-ci.
 		// Ecrivez ensuite le nom et l'âge de cette personne sur la sortie standard.
+
+		Person p = new Person(args[0], Integer.parseInt(args[1]));
+		System.out.print(p.getName() + p.getAge());
+
 	}
 }
--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,35 @@
 package exercise1;
 
+import java.util.Scanner;
+
 class Person {
+
+	private String name;
+	private int age;
+
+	public Person()
+	{
+		Scanner cin = new Scanner(System.in);
+		System.out.print("Nom: ");
+		this.name = cin.next();
+		System.out.print("Age: ");
+		this.age = cin.nextInt();
+	}
+
+	public Person(String name,int age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+	public int getAge()
+	{
+		return this.age;
+	}
+
 }
