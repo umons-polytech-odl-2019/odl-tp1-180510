dfa59542176c9a8ed4f31db35cb169d3f5e405ad
180510 <180510@umons.ac.be>
2019-10-23 15:16:56 +0200

    Exercice 1 Séance TP

--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,4 +1,28 @@
 package exercise2;
 
 class Person {
+	private static int count = 0;
+	private static int sumAges = 0;
+	private int age;
+
+	public Person(int age)
+	{
+		this.age = age;
+		this.sumAges += age;
+		this.count++;
+	}
+
+	public static int computePopulationSize() {
+		return count;
+	}
+
+	public static float computeAveragePopulationAge() {
+		return (float)sumAges/count;
+	}
+
+	public static void resetPopulation() {
+		count = 0;
+		sumAges = 0;
+	}
+
 }
